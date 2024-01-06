4babe456f23c156f1ffef9a21be2a42652228f87
pawangupta5050 <85182279+pawangupta5050@users.noreply.github.com>
2024-01-06 14:09:47 +0530

    Minor change

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -5,7 +5,7 @@ const ProjectsSideBar = ({addProjectHandler, projectList, onSelectProject, selec
   console.log(projectList)
   return ( 
     <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl'>
-        <h2 className='mb-8 font-bold uppercase md:text-xl text-stone-200'>Your Projects</h2>
+        <h2 className='mb-8 font-bold uppercase md:text-xl text-stone-300'>Your Projects</h2>
         <div>
             <Button onClick={addProjectHandler}>+ Add Project</Button>
         </div>
